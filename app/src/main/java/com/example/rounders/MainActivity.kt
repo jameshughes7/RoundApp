6ec915b10cc6390759543e05e8c969ddb92e6443
James Hughes <hugjimbo77@gmail.com>
2019-01-29 15:12:14 +0000

    override doInBackground function in prep for downloading data

--- app/src/main/java/com/example/rounders/MainActivity.kt
+++ app/src/main/java/com/example/rounders/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.rounders
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate Called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.text = "Rounders App started!"
-
         button.setOnClickListener { toast("Message: ${input.text}") }
+        Log.d(TAG, "onCreate Ends")
     }
 
     fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
